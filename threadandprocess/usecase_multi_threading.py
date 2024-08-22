d001d246f11a4a014864ad2a5cee646ec292146c
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-08-22 15:33:24 +0100

    done with multithreadin

---
+++ threadandprocess/usecase_multi_threading.py
@@ -0,0 +1,8 @@
+"""
+Real-World Example: Multithreading for I/O - bound Tasks Scenario: 
+Web Scraping
+Web scraping often involves making numberous network requests to fetch web pages. 
+These tasks are I/O bound bec ause they spend
+a lot of time waiting for responses from servers. Multithreading can significantly 
+improve the performance by allowing multiple web pages to be fetched concurrently.
+"""
